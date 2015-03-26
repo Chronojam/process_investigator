0df317583cf342ace08a4309f358ff67c902d930
Chronojam <Knottedhair@gmail.com>
2015-03-26 00:05:56 +0000

    Update gatherer.sh

--- gatherer.sh
+++ gatherer.sh
@@ -9,6 +9,6 @@ COUNT=$2
 TOTAL_MEM=$(free -m | grep Mem: | cut -c14-18)
 top -n$COUNT -b -p $PID | grep "$PID" | cut -c50-57  > /tmp/.usage_dump_$DATESTAMP.txt
 
-./process_investigator_mod.py $LOG_NAME $TOTAL_MEM
+./parser.py $LOG_NAME $TOTAL_MEM
 echo "Done!"
 rm $LOG_NAME
